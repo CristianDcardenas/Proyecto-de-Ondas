a36644c7447fa4168bebe32478e8ba9c08bf69f4
Cristian David Cardenas Murillo <cardenasmurillocristiandavid@gmail.com>
2025-06-01 21:25:35 -0500

    Ya esta listo

--- Proyecto de Ondas/BotConfig.cs
+++ Proyecto de Ondas/BotConfig.cs
@@ -8,13 +8,13 @@ namespace Proyecto_de_Ondas
 
         private static string GetBotToken()
         {
-            // Intentar obtener del entorno primero (más seguro)
+           
             var token = Environment.GetEnvironmentVariable("TELEGRAM_BOT_TOKEN");
 
             if (!string.IsNullOrEmpty(token))
                 return token;
 
-            // Token por defecto (cambiar por el tuyo)
+           
             return "7772298308:AAFCmedWSYxQsasr4F1xXGmOYVem_W_EqSU";
         }
     }
